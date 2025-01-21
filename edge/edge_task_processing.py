10baeb583cd34ed594d542b86f7a9459cbb9c40b
edge <yuanliang123@hotmail.com>
2025-01-21 16:24:02 +1030

    testing

--- edge/edge_task_processing.py
+++ edge/edge_task_processing.py
@@ -341,7 +341,13 @@ def connect_mqtt():
         client.connect(MQTT_BROKER, MQTT_PORT, keepalive=60)
         client.subscribe(MQTT_TOPIC_AGGREGATED)
         print(f"[{DEVICE_ID}] Subscribed to {MQTT_TOPIC_AGGREGATED}")
-        client.publish(MQTT_TOPIC_UPLOAD, 'testing message')
+        payload = json.dumps({
+            'device_id': DEVICE_ID,
+            'model_type': 'model_type',
+            'model_data': 'model_b64',
+            'data_type': 'data_tpye'
+        })
+        client.publish(MQTT_TOPIC_UPLOAD, payload)
         print(f"[{DEVICE_ID}] Sent testing message to {MQTT_TOPIC_UPLOAD}")
     except Exception as e:
         logger.exception(f"Failed to connect to MQTT broker: {e}")
